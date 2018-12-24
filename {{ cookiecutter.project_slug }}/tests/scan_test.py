af8bc51affc4504dd6934431b1da0c480a58e459
Nick Davis <nick.davis@polyswarm.io>
2018-12-24 14:41:15 -0500

    fixed syntax bug. added documentation about asyncio loop requirements.

--- {{ cookiecutter.project_slug }}/tests/scan_test.py
+++ {{ cookiecutter.project_slug }}/tests/scan_test.py
@@ -11,7 +11,10 @@ from {{ cookiecutter.package_slug }} import Microengine, Scanner
 @pytest.yield_fixture()
 def event_loop():
     """
-    To enable Windows engines to support subprocesses, they need to use the ProactorEventLoop.
+    To enable Windows engines to support subprocesses in asyncio, they need to use the ProactorEventLoop.
+
+    When the engine is running via polyswarm-client, this is handled by polyswarm-client.
+    But when using pytest, you do not run the engine via polyswarm-client, so you have to change the loop in a fixture.
 
     ref: https://docs.python.org/3/library/asyncio-eventloops.html
 
@@ -31,9 +34,9 @@ async def test_scan_random_mal_not():
 
     """
     scanner = Scanner()
-    { % if cookiecutter.has_backend == "true" %}
+    {% if cookiecutter.has_backend == "true" %}
     scanner.wait_for_backend()
-    { % endif %}
+    {% endif %}
 
     for t in [True, False]:
         mal_md, mal_content = DummyMalwareRepoClient().get_random_file(malicious_filter=t)
