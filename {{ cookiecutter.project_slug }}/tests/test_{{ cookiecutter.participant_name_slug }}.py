4d403038b63a9042447a52660442b3af5c61233e
Javier Botella <javier.botella@polyswarm.io>
2019-08-22 11:57:28 +0200

    code style - PEP8 Blank Lines

--- {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.participant_name_slug }}.py
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.participant_name_slug }}.py
@@ -4,12 +4,13 @@
 import asyncio
 import pytest
 
-{% if cookiecutter.participant_type == "ambassador" %}
+{%- if cookiecutter.participant_type == "ambassador" -%}
 from {{ cookiecutter.package_slug }} import Ambassador
-{% endif %}
+{%- endif -%}
 
 {% if cookiecutter.participant_type == "microengine" or cookiecutter.participant_type == "arbiter" %}
 import sys
+
 from malwarerepoclient.client import DummyMalwareRepoClient
 from {{ cookiecutter.package_slug }} import Scanner
 from polyswarmartifact import ArtifactType
@@ -37,22 +38,20 @@ def event_loop():
 @pytest.mark.asyncio
 async def test_scan_random_mal_not():
     """
-{% if cookiecutter.microengine__supports_scanning_files == "true" %}
+{%- if cookiecutter.microengine__supports_scanning_files == "true" %}
     1. Run scanner against one malicious file (EICAR) and one non-malicious file.
-{% endif %}
+{%- endif -%}
 {% if cookiecutter.microengine__supports_scanning_urls == "true" %}
     2. Run scanner against one malicious URL and non-malicious URL.
 {% endif %}
     """
     scanner = Scanner()
     await scanner.setup()
-
 {% if cookiecutter.microengine__supports_scanning_files == "true" %}
-
     ###
     ### File artifacts
     ###
-    
+
     for t in [True, False]:
         mal_md, mal_content = DummyMalwareRepoClient()\
                               .get_random_file(malicious_filter=t)
@@ -63,32 +62,30 @@ async def test_scan_random_mal_not():
                                     chain='home')
         assert result.bit
         assert result.verdict == t
-    
-{% endif %}
+{% endif -%}
 
 {% if cookiecutter.microengine__supports_scanning_urls == "true" %}
-
     ###
     ### URL artifacts
     ###
 
     # Expect malicious
     url = b'http://iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com'
-    result = await scanner.scan('nocare', ArtifactType.URL, 
+    result = await scanner.scan('nocare', ArtifactType.URL,
                                 url, None, 'home')
     assert result.bit
     assert result.verdict
 
     # Except benign
     url = b'https://google.com'
-    result = await scanner.scan('nocare', ArtifactType.URL, 
+    result = await scanner.scan('nocare', ArtifactType.URL,
                                 url, None, 'home')
     assert result.bit
     assert not result.verdict
 
-{% endif %}
+{%- endif -%}
 
-{% endif %}
+{%- endif -%}
 
 {% if cookiecutter.participant_type == "ambassador" %}
 
@@ -100,5 +97,4 @@ async def todo():
     TODO
     """
     assert True is True
-
-{% endif %}
+{%- endif -%}
